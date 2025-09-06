d9fc70ee901127082001e7cd9a86ec143cc87809
Kevin Gisi <kevin.gisi@calendly.com>
2025-09-06 15:38:46 -0500

    Add dynamic haproxy routing

--- modules/fort/registry-coredns-subscriber/listener.rb
+++ modules/fort/registry-coredns-subscriber/listener.rb
@@ -2,6 +2,7 @@ require 'redis'
 require 'json'
 
 HOSTS_FILE = "/etc/coredns/hosts.conf"
+HAPROXY_FILE = "/var/lib/haproxy/dynamic.cfg"
 
 @redis = Redis.new(path: @registry_sock)
 @domains = {}
@@ -11,6 +12,50 @@ def write_domains_to_hostfile
     @domains.each do |domain, opts|
       f.puts("#{opts["ip"]}\t#{domain}")
     end
+  end
+end
+
+def write_haproxy_router
+  ip_to_domains = @domains.group_by { |_domain, opts| opts["ip"] }
+
+  frontend_entries = []
+  backend_entries = []
+
+  ip_to_domains.each_with_index do |(ip, domain_pairs), i|
+    backend_name = "backend#{i}"
+
+    domain_pairs.each do |domain, _|
+      frontend_entries << "  use_backend #{backend_name} if { req_ssl_sni -i #{domain} }"
+    end
+
+    backend_entries << <<~BACKEND
+      backend #{backend_name}
+        mode tcp
+        server #{backend_name}_srv #{ip}:443
+    BACKEND
+  end
+
+  config = <<~HAPROXY
+    frontend https-in
+      bind *:443
+      tcp-request inspect-delay 5s
+      tcp-request content accept if { req_ssl_hello_type 1 }
+
+  #{frontend_entries.join("\n")}
+
+  #{backend_entries.join("\n")}
+  HAPROXY
+
+  File.write(HAPROXY_FILE, config)
+
+  if system("systemctl is-active --quiet haproxy")
+    puts "Reloading haproxy"
+    system("systemctl reload haproxy")
+  elsif system("systemctl is-enabled --quiet haproxy")
+    puts "Starting haproxy"
+    system("systemctl start haproxy")
+  else
+    puts "haproxy not enabled; skipping"
   end
 end
 
@@ -25,6 +70,7 @@ loop do
 end
 
 write_domains_to_hostfile
+write_haproxy_router
 
 @redis.subscribe("updates") do |on|
   on.message do |channel, message|
@@ -34,6 +80,7 @@ write_domains_to_hostfile
     if @domains[key] != value
       @domains[key] = value
       write_domains_to_hostfile
+      write_haproxy_router
     end
   end
 end
