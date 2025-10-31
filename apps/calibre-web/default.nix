7c20f13f907bd9d52048b37a49e9fcda18870926
Kevin Gisi <kevin.gisi@calendly.com>
2025-10-31 11:13:41 -0500

    Add calibre-web

---
+++ apps/calibre-web/default.nix
@@ -0,0 +1,62 @@
+{ ... }:
+{ config, lib, pkgs, ... }:
+let
+  stateDir = "/var/lib/calibre-web";
+in
+{
+  users.groups = {
+    media = lib.mkDefault { };
+    calibre-web = { };
+  };
+
+  users.users.calibre-web = {
+    isSystemUser = true;
+    group = "calibre-web";
+    extraGroups = [ "media" ];
+    home = stateDir;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${stateDir} 0750 calibre-web calibre-web -"
+  ];
+
+  systemd.services.calibre-web = {
+    description = "Calibre Web";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" "zfs-late-import.service" ];
+
+    serviceConfig = {
+      User = "calibre-web";
+      Group = "calibre-web";
+      WorkingDirectory = stateDir;
+      RuntimeDirectory = "calibre-web";
+      Restart = "on-failure";
+      RestartSec = 10;
+      ExecStart = lib.concatStringsSep " " [
+        "${pkgs.calibre-web}/bin/calibre-web"
+        "-p ${stateDir}/app.db"
+        "-g ${stateDir}/gdrive.db"
+        "-i 127.0.0.1"
+        "-o ${stateDir}/calibre-web.log"
+      ];
+      Environment = [
+        "CALIBRE_DBPATH=${stateDir}"
+        "CALIBRE_LIBRARY=/media/ebooks/metadata.db"
+      ];
+    };
+
+    path = [
+      pkgs.calibre
+      pkgs.util-linux
+      pkgs.coreutils
+    ];
+  };
+
+  fortCluster.exposedServices = [
+    {
+      name = "calibre-web";
+      subdomain = "ebooks";
+      port = 8083;
+    }
+  ];
+}
--- hosts/ursula/manifest.nix
+++ hosts/ursula/manifest.nix
@@ -7,6 +7,7 @@ rec {
   apps = [
     "jellyfin"
     "audiobookshelf"
+    "calibre-web"
   ];
 
   aspects = [
