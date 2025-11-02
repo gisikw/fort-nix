d91b022f68c6ee1d3ebfdf8e8026e07c5357e827
Kevin Gisi <kevin.gisi@calendly.com>
2025-11-02 14:18:30 -0600

    Service registry cleanup

--- aspects/service-registry/default.nix
+++ aspects/service-registry/default.nix
@@ -35,7 +35,7 @@ in
       peers=$(echo $mesh | jq -r --arg user "$user" '.Peer | to_entries[] | select(.value.UserID == ($user | tonumber)) | .value.DNSName')
       host_lan_ip=$(ip -4 route get 1.1.1.1 | awk '{for (i=1; i<=NF; i++) if ($i=="src") print $(i+1)}')
 
-      entries=$(
+      services=$(
         for peer in ${hostManifest.hostName}.fort.${domain} $peers; do
           output=$(ssh $SSH_OPTS -n "root@$peer" "
             ip -4 -o addr show fortmesh0 2>/dev/null | head -n1 || echo 'NOFORT'
@@ -43,23 +43,19 @@ in
             cat /var/lib/fort/services.json 2>/dev/null || echo '[]'
           ")
 
-          mesh_ip_line=$(echo "$output" | sed -n '1p')
+          vpn_ip_line=$(echo "$output" | sed -n '1p')
           lan_ip_line=$(echo "$output" | sed -n '2p')
           services=$(echo "$output" | sed -n '3,$p')
 
-          mesh_ip=$(echo "$mesh_ip_line" | awk '{print $4}' | cut -d/ -f1)
+          vpn_ip=$(echo "$vpn_ip_line" | awk '{print $4}' | cut -d/ -f1)
           lan_ip=$(echo "$lan_ip_line" | awk '{for (i=1; i<=NF; i++) if ($i=="src") print $(i+1)}')
 
           echo "$services" | jq \
             --arg peer "$peer" \
-            --arg mesh_ip "$mesh_ip" \
+            --arg vpn_ip "$vpn_ip" \
             --arg lan_ip "$lan_ip" \
-            '.[] |= . + { hostname: $peer, mesh_ip: $mesh_ip, lan_ip: $lan_ip }'
-        done | jq -s 'add'
-      )
-
-      echo $entries \
-        | jq '
+            '.[] |= . + { hostname: $peer, vpn_ip: $vpn_ip, lan_ip: $lan_ip }'
+        done | jq -s 'add' | jq '
             map(
               if has("subdomain") and (.subdomain != null) then
                 . + { fqdn: (.subdomain + ".${domain}") }
@@ -67,24 +63,15 @@ in
                 . + { fqdn: (.name + ".${domain}") }
               end
             )
-          ' \
-        | jq 'map({ name: .fqdn, type: "A", value: .mesh_ip })' \
+        '
+      )
+
+      echo $services \
+        | jq 'map({ name: .fqdn, type: "A", value: .vpn_ip })' \
         | ssh $SSH_OPTS "root@${beaconHost}.fort.${domain}" "tee /var/lib/headscale/extra-records.json >/dev/null"
 
-      echo $entries \
-        | jq -r \
-          --arg dom "${domain}" \
-          '
-            .[]
-            | select(.visibility != "vpn")
-            | "\(.lan_ip) \(
-                if has("subdomain") and (.subdomain != null) then
-                  (.subdomain + "." + $dom)
-                else
-                  (.name + "." + $dom)
-                end
-              )"
-          ' \
+      echo $services \
+        | jq -r '.[] | select(.visibility != "vpn") | "\(.lan_ip) \(.fqdn)"' \
         | tee /var/lib/coredns/custom.conf >/dev/null
     '';
     serviceConfig = {
