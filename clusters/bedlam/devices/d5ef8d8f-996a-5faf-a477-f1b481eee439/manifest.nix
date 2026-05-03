7c9340120380c325d3e90487d2087d1518ee199e
Kevin Gisi <kevin.gisi@calendly.com>
2026-05-02 23:29:28 -0500

    Add new frankenstein device

---
+++ clusters/bedlam/devices/d5ef8d8f-996a-5faf-a477-f1b481eee439/manifest.nix
@@ -0,0 +1,6 @@
+{
+  uuid = "d5ef8d8f-996a-5faf-a477-f1b481eee439";
+  profile = "xeon-workstation";
+  pubkey = ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMgOJ2CV1H7kaj9RVm6JC4kPKkChSzuAEAIG26znyRr fort-device-d5ef8d8f-996a-5faf-a477-f1b481eee439'';
+  stateVersion = ''26.05'';
+}
