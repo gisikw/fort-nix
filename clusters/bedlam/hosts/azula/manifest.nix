7f490e613854fdf024092640ce25dd22885d1ef1
Kevin Gisi <kevin@kevingisi.com>
2026-06-18 02:59:32 +0000

    azula: add captive portal tools
    Install Firefox, XFCE/LightDM, text browsers, and network debugging tools so azula can complete office captive portal registration before it has reliable network access.

--- clusters/bedlam/hosts/azula/manifest.nix
+++ clusters/bedlam/hosts/azula/manifest.nix
@@ -9,8 +9,26 @@ rec {
   aspects = [ "observable" ];
 
   module =
-    { config, ... }:
+    { config, pkgs, ... }:
     {
       config.fort.host = { inherit roles apps aspects; };
+
+      # Office captive-portal survival kit. Azula may need to register on
+      # unfamiliar networks before it can fetch anything else, so keep both a
+      # graphical browser path and text-mode/debug tools available locally.
+      config.services.xserver.enable = true;
+      config.services.xserver.displayManager.lightdm.enable = true;
+      config.services.xserver.desktopManager.xfce.enable = true;
+
+      config.environment.systemPackages = with pkgs; [
+        firefox
+        w3m
+        lynx
+        curl
+        wget
+        dnsutils
+        openssl
+        xterm
+      ];
     };
 }
